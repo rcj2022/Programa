01afc5f8183412ea7a56971ec7d83e166b5b374b
RomelCardoso <romelcardosojesus@gmail.com>
2023-07-04 17:57:38 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,22 @@
 <?php
+<?php 
+$dbhost = 'localhost';
+$username = 'root';
+$pwd = '';
+$dbname = 'prog';
 
+
+$con = new mysqli($dbhost,$username,$pwd,$dbname);
+
+if($con->connect_errno){
+    echo "Erro";
+}
+else {
+    echo "Conexão efetuada com sucersso"; 
+}
+
+
+?>
 
 
 
