7efcb84a397067261156dac511313e0b5c08579e
RomelCardoso <romelcardosojesus@gmail.com>
2023-07-05 10:59:00 -0300

    atualizar

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="style.css">
+
+    <title>Home</title>
+   
+  </head>
+<body>
+
+<div class="box">
+
+    <form action="">
+        <fieldset>
+            <legend><b>Cadastro</b></legend>
+            <br>
+            <div class ="inputData">
+            <label for="data" class="data">Data</label> 
+            <input type="date" name="data" id="data" placeholder="Digite a data..." class="data">
+            </div>
+            <br>
+            <div class ="inputPre">
+            <label for="presidente">Presidente</label> 
+            <input type="text" name="presidente" id="presidente" placeholder="Digite o nome do presidente..." class ="presid">
+            </div>
+
+
+          
+      
+        </fieldset>
+        <br>
+        <input type="submit" value="Cadastrar" class="botao">
+    </form>
+
+</div>
+    
+</body>
+</html>
